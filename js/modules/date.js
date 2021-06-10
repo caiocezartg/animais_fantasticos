0d15e6afdf42d37d90e3cfe02e933281b25227b7
Caio <caiocezartg@gmail.com>
2021-06-10 16:49:51 -0300

    Data de Funcionamento Refatorado

--- js/modules/date.js
+++ js/modules/date.js
@@ -1,17 +1,39 @@
-export default function initDate(){
+export default class Funcionamento{
+    constructor(funcionamento){
+        this.funcionamento = document.querySelector(funcionamento); //Selecionou as datas
+    }
 
-}
+    dadosFuncionamento(){
+        this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number); //Transforma em array através do split e cada item que seja String em um Number através do map;
+        this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number); //Transforma em array através do split e cada item que seja String em um Number através do map;
+    }
 
-const funcionamento = document.querySelector('[data-semana]'); //Selecionou as datas
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number); //Transforma em array através do split e cada item que seja String em um Number através do map;
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number); //Transforma em array através do split e cada item que seja String em um Number através do map;
+    dadosAgora(){
+        this.dataAgora = new Date();
+        this.diaAgora = this.dataAgora.getDay(); //Retorna o dia de agora em número
+        this.horarioAgora = this.dataAgora.getUTCHours() - 3; //Retorna a hora de agora em número
+    }
 
-const dataAgora = new Date();
-const diaAgora = dataAgora.getDay(); //Retorna o dia de agora em número
-const horarioAgora = dataAgora.getHours(); //Retorna a hora de agora em número
+    estaAberto(){
+        this.semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1 //Verifica se o dia de hoje (agora) está contido na array dos dias da semana (diasSemana), se não tiver, o indexOf retorna -1
+        this.horarioAberto = (this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1]); //Verifica se o horario está entre o primeiro e segundo item da array, as quais são 2 horarios em números.
 
-const semanaAberto = diasSemana.indexOf(diaAgora) !== -1 //Verifica se o dia de hoje (agora) está contido na array dos dias da semana (diasSemana), se não tiver, o indexOf retorna -1
-const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]); //Verifica se o horario está entre o primeiro e segundo item da array, as quais são 2 horarios em números.
-if(semanaAberto && horarioAberto){
-    funcionamento.classList.add('aberto');
+        return this.semanaAberto && this.horarioAberto;
+    }
+
+    ativaAberto(){
+        if (this.estaAberto()){
+            this.funcionamento.classList.add('aberto');
+        }
+    }
+
+    init(){
+        if (this.funcionamento){
+            this.dadosFuncionamento();
+            this.dadosAgora();
+            this.ativaAberto();
+        }
+        return this;
+    }
+
 }
--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initDate from './modules/date.js';
 import fetchAnimais from './modules/fetchanimais.js';
 import FetchBitcoin from './modules/fetchbitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
 import DropdownMenu from './modules/dropdownmenu.js';
 import MenuMobile from './modules/menu-mobile.js';
+import Funcionamento from './modules/date.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -28,9 +28,12 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
+const funcionamento = new Funcionamento('[data-semana]');
+funcionamento.init();
+
 initAccordion();
 initTabNav();
-initDate();
+
 
 fetchAnimais('./animais.json', '.numeros-grid');
 FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
