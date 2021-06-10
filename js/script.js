34b2f015b2f339d68710f63c746effadb73fffda
Caio <caiocezartg@gmail.com>
2021-06-10 00:05:39 -0300

    Modal Refatorado

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import Modal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdownmenu.js'
 import initMenuMobile from './modules/menu-mobile.js'
@@ -13,10 +13,13 @@ import initFetchBitcoin from './modules/fetchbitcoin.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
+
+
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile()
